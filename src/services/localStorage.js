18c24633e7305d247e8688988d82a2d51e6ebf30
Celia Martin Herranz <celiamartinherranz11@gmail.com>
2022-12-14 12:32:29 +0100

    update

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,6 +1,3 @@
-// File src/services/localStorage.js
-
-// Function to get a property from local storage
 const get = (key, defaultValue) => {
     const localStorageData = localStorage.getItem(key);
     if (localStorageData === null) {
@@ -10,23 +7,19 @@ const get = (key, defaultValue) => {
     }
 };
 
-//Function to save a property and its value in local storage 
 const set = (key, value) => {
     const localStorageData = JSON.stringify(value);
     localStorage.setItem(key, localStorageData);
 };
 
-//Function to erase a property from local storage
 const remove = (key) => {
     localStorage.removeItem(key);
 };
 
-//Fucntion to clean all local storage 
 const clear = () => {
     localStorage.clear();
 };
 
-//Create object to export
 const objectToExport = {
     get: get,
     set: set,
@@ -34,5 +27,4 @@ const objectToExport = {
     clear: clear,
 };
 
-//Export object to use it in App component
 export default objectToExport;
