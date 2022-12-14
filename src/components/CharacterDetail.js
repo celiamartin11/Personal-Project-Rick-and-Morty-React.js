fb4fa88392c007e1f54ed5c3dc1c0937bb432100
Celia Martin Herranz <celiamartinherranz11@gmail.com>
2022-12-14 09:38:06 +0100

    add update

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 function CharacterDetail (props) {
     return <main className='contain_detail'>
     <section className='character_detail'>
-        <h2 className='name_detail'>{props.character.name}</h2>
-        <p className='name'>{props.character.specie}</p>
-        <img className='image' src={props.character.img} alt={props.character.name}/>
+        <div className='info_detail'>
+            <h2 className='name_detail'>{props.character.name}</h2>
+            <p className='specie_detail'>{props.character.specie}</p>
+        </div>
+        <img className='image_detail' src={props.character.img} alt={props.character.name}/>
         <Link to="/" className='link_detail'>Inicio</Link>
     </section>
     </main>
